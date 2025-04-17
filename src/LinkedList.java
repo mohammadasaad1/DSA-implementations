1ab5a62a9d93df1dce8ea2b950e0afd2c88e29fb
mohammadasaad1 <mohammadfoqha2@hotmail.com>
2025-04-17 14:05:14 +0300

    create essintial methods linked list needs

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class LinkedList {
     private class Node {
         int data;
@@ -8,17 +10,85 @@ public class LinkedList {
     }
     private Node first;
     private Node last;
+    private int size;
 
+    public boolean isEmpty() {
+        return first == null;
+    }
     public void addLast(int data) {
         var node = new Node(data);
 
-        if(first == null)
+        if(isEmpty())
             first = last = node ;
-
         else {
             last.next = node;
             last = node;
         }
+        size++;
     }
-
+    public void addFirst(int data) {
+        var node = new Node(data);
+        if (isEmpty())
+            first = last = node;
+        else{
+            node.next = first;
+            first = node;
+        }
+        size++;
+    }
+    public int indexOf(int data){ int index = 0;
+        var current = first ;
+        while (current != null) {
+            if(current.data == data) return index;
+            current = current.next;
+            index++;
+        }
+        return -1; // if we don't find the item here.
+    }
+    public boolean contains(int data){
+        return indexOf(data) != -1;
+    }
+    public void removeFirst(){
+        if(isEmpty()) throw new NoSuchElementException();
+        if(first == last){
+            first = last = null;
+            return;
+        }
+        var second = first.next;
+        first.next = null;
+        first = second;
+        size--;
+    }
+    public void removeLast(){
+        if(isEmpty()) throw new NoSuchElementException();
+        if(first == last){
+            first = last = null ;
+            return;
+        }
+        var previous = getPrevious(last) ;
+        last = previous ;
+        last.next = null;
+        size--;
+    }
+    private Node getPrevious(Node node){
+        var current = first ;
+        while(current != null){
+            if(current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+    public int getSize(){
+        return size;
+    }
+    public int[] toArray(){
+        int index = 0 ;
+        var array = new int[size] ;
+        var current = first ;
+        while(current != null){
+            array[index++] = current.data ;
+            current = current.next ;
+        }
+        return array ;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         LinkedList x = new LinkedList();
         x.addLast(1);
         x.addLast(2);
         x.addLast(3);
+        var arr = x.toArray() ;
+        System.out.println(Arrays.toString(arr));
     }
 }
